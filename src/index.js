2dbcb2d1a89fb98ada406181037172ff6100ceb1
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-04-30 07:11:13 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./Components/App";
+import "semantic-ui-css/semantic.min.css";
+import './index.css';
+
+createRoot(document.getElementById('root')).render(<App />);
+
