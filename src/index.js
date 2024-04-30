25b0d6f0b25f2b26c1a82e6ce7f0c4a0b5ee470d
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-04-30 07:10:18 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom";
-import App from "./Components/App";
-import "semantic-ui-css/semantic.min.css";
-import './index.css';
-
-createRoot(document.getElementById('root')).render(<App />);
